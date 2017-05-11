cdec7f67f2f7265771a05694857c1bbf0c6cb6e9
JacobWylie <jacobwylie@gmail.com>
2017-05-11 17:55:39 +0200

    hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
+
 module.exports = {
-	entry: './app.jsx',
-
-	output: {
-		// path: 'build',
-		filename: 'bundle.js'
-	},
-
-	module: {
-		loaders: [
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		]
-	}
+  entry: './app.js',
+  
+  output: {
+    // path: 'build',
+    filename: 'bundle.js'
+  },
+  
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', 'babel-loader'] 
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader' 
+      }
+    ]
+  }
 };
